2593a73a2458b545c667805c68837470d8dc7c1f
AJGeo <aj_bresler@yahoo.co.uk>
2019-10-06 01:29:23 +0200

    GHappy update

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -3,11 +3,24 @@ package tudelft.ghappy;
 public class GHappy {
 
     public boolean gHappy(String str) {
-        assert str!=null;
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+        assert str != null;
+
+//        // If the input string does not contain any "gg" why bother looking any further, using java string function
+//        if(!str.contains("gg"))
+//            return false;
+
+        // if input string length is less than 2, then true condition is impossible
+        if (str.length() < 2)
+            return false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+                if (i > 0 && str.charAt(i - 1) == 'g') {
+                    continue;
+                }
+                if (i + 1 < str.length() && str.charAt(i + 1) == 'g') {
+                    continue;
+                }
                 return false;
             }
         }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,21 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "name={0}: ({1}) = result={2}")
+    @CsvSource({
+            // Provided samples
+            "Samples,xxggxx,true", "Samples,xxgxx,false", "Samples,xxggyygxx,false",
+            // No g variants
+            "No g,abdef,false", "One g,g,false", "2 g,gg,true",
+            // Funnies
+            "Empty,'',false", "Null,,false"
+    })
+    public void testGHappy(String partition, String testSr, boolean expectedResult) {
+        boolean result = new GHappy().gHappy(testSr);
+        Assertions.assertTrue(expectedResult == result);
+    }
 }
