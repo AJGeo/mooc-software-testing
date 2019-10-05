8bb2d6d95b1a07cc307fab723cc0dc67fe793bfc
AJGeo <aj_bresler@yahoo.co.uk>
2019-10-06 01:28:54 +0200

    Mirror update

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -5,17 +5,22 @@ public class Mirror {
     public String mirrorEnds(String string) {
         String mirror = "";
 
+        // If null string return empty string
+        if (string == null)
+            return mirror;
+
         int begin = 0;
         int end = string.length() - 1;
         for (; begin < end; begin++, end--) {
             if (string.charAt(begin) == string.charAt(end)) {
                 mirror += String.valueOf(string.charAt(end));
-            }
-            else {
+            } else {
                 break;
             }
         }
 
-        return begin == end ? string : mirror;
+        // If begin==end an uneven length string and every char was tested to the middle of string with all matching
+        // If begin>=end an even length string and every char was tested to the past the middle of string with all matching
+        return begin >= end ? string : mirror;
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,22 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+    @ParameterizedTest(name = "name={0}: ({1}) = result={2}")
+    @CsvSource({
+            // Provided samples
+            "Samples,abXYZba,ab", "Samples,abca,a", "Samples,aba,aba",
+            // No mirrors
+            "No Mirror,abdefg,''","One char,a,a","2 Char,aa,aa",
+            // Funnies
+            "Space, asdfa , a ","Empty,'',''","Null,,''",""
+    })
+    public void testMirror(String partition, String testSr, String expectedResult) {
+        String result = new Mirror().mirrorEnds(testSr);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
