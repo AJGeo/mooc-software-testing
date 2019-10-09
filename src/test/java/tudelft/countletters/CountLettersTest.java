db26e4bbd6f1c74de00394b5e4c0886faaa1a67e
AJGeo <aj_bresler@yahoo.co.uk>
2019-10-09 23:17:12 +0200

    CountLetters tests and bug fixing

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -2,19 +2,38 @@ package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountLettersTest {
 
-    @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
-        Assertions.assertEquals(2, words);
+    @ParameterizedTest(name = "name={0}: ({1}) = result={2}")
+    @CsvSource({
+            // Provided samples
+            "Multiple S,cats|dogs,2", "Multiple R,catr|dogr,2"
+    })
+    public void multipleMatchingWords(String partition, String testSr, int expectedResult) {
+        int words = new CountLetters().count(testSr);
+        Assertions.assertEquals(expectedResult, words);
     }
 
-    @Test
-    public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
-        Assertions.assertEquals(1, words);
+    @ParameterizedTest(name = "name={0}: ({1}) = result={2}")
+    @CsvSource({
+            // Provided samples
+            "Last not S,cats|dog,1", "Last not R,catr|dog,1"
+    })
+    public void lastWordDoesNotMatch(String partition, String testSr, int expectedResult) {
+        int words = new CountLetters().count(testSr);
+        Assertions.assertEquals(expectedResult, words);
     }
 
+    @ParameterizedTest(name = "name={0}: ({1}) = result={2}")
+    @CsvSource({
+            // Provided samples
+            "First not S,cat|dogs,1", "First not R,cat|dogr,1"
+    })
+    public void firstWordDoesNotMatch(String partition, String testSr, int expectedResult) {
+        int words = new CountLetters().count(testSr);
+        Assertions.assertEquals(expectedResult, words);
+    }
 }
