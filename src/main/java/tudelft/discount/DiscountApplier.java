dbdbd54b002596df4b7a769ef9207d913b567cf2
AJGeo <aj_bresler@yahoo.co.uk>
2019-10-15 00:46:54 +0200

    DiscountApplier Test and Fix

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -1,23 +1,31 @@
 package tudelft.discount;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
+
 public class DiscountApplier {
 
     private ProductDao dao;
 
-    public DiscountApplier (ProductDao dao) {
+    public DiscountApplier(ProductDao dao) {
         this.dao = dao;
     }
 
     public void setNewPrices() {
-
-        for(Product product : dao.all()) {
-            if(product.getCategory().equals("BUSINESS")) {
-                product.setPrice(product.getPrice() * 0.9);
+        for (Product product : dao.all()) {
+            if (product.getCategory().equals("BUSINESS")) {
+                product.setPrice(roundNumber(product.getPrice() * 1.1));
             }
-            if(product.getCategory().equals("HOME")) {
-                product.setPrice(product.getPrice() * 1.1);
+            if (product.getCategory().equals("HOME")) {
+                product.setPrice(roundNumber(product.getPrice() * 0.9));
             }
         }
+    }
 
+    private double roundNumber(double value) {
+        BigDecimal bigDecimal = new BigDecimal(Double.toString(value));
+        bigDecimal = bigDecimal.setScale(2, RoundingMode.HALF_UP);
+        return bigDecimal.doubleValue();
     }
 }
--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,35 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountApplierTest {
+    private ProductDao dao;
+
+    @BeforeEach
+    public void initialize() {
+        this.dao = Mockito.mock(ProductDao.class);
+    }
+
+    @Test
+    public void discountApplierTest() {
+        Product compABC = new Product("ABC Business", 100, "BUSINESS");
+        Product homeMe = new Product("Mr Doo", 100, "HOME");
+        Product govHol = new Product("Holland", 100, "GOVERNMENT");
+
+        List<Product> results = Arrays.asList(compABC, homeMe, govHol);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier discountApplier = new DiscountApplier(dao);
+        discountApplier.setNewPrices();
+
+        Assertions.assertEquals(110, compABC.getPrice());
+        Assertions.assertEquals(90, homeMe.getPrice());
+        Assertions.assertEquals(100, govHol.getPrice());
+    }
 }
